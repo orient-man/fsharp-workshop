de6ff4bbfdcfdd435eec6217b5e7e4e77322af56
Marcin Malinowski <orientman@gmail.com>
2017-03-24 16:16:46 +0100

    Yay! First unit test written in F#

---
+++ 02_UnitTesting/App.Tests/AppTests.fs
@@ -0,0 +1,7 @@
+﻿module App.Tests
+
+open NUnit.Framework
+
+[<Test>]
+let ``2 + 2 = 4`` () =
+    Assert.AreEqual(4, 2 + 2)
---
+++ 02_UnitTesting/build.fsx
@@ -0,0 +1,21 @@
+#r "./packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+open Fake.Testing
+
+let buildDir = "./build/"
+let appReferences = !! "**/*.csproj" ++ "**/*.fsproj"
+
+Target "Clean" (fun _ -> CleanDirs [buildDir])
+
+Target "Build" (fun _ ->
+    appReferences
+    |> MSBuildDebug buildDir "Build"
+    |> Log "Build-output: "
+)
+
+Target "Test" (fun _ -> !! (buildDir + "*.Tests.dll") |> NUnit3 id)
+
+"Clean" ==> "Build" ==> "Test"
+
+RunTargetOrDefault "Test"
